45e4f43d4894930a51b7cf82ba0273cbe228aff8
Daniela Petrova <dani@robotev.com>
2019-02-13 15:55:21 +0200

    queries, mutations, routing and authentication

---
+++ src/main.js
@@ -0,0 +1,60 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloLink } from 'apollo-link'
+import Vue from 'vue'
+import VueApollo from 'vue-apollo'
+import App from './App'
+import router from './router'
+import 'tachyons'
+import { GC_USER_ID, GC_AUTH_TOKEN } from './constants/settings'
+
+Vue.config.productionTip = false
+
+const httpLink = new HttpLink({
+  // You should use an absolute URL here
+  uri: 'https://api.graph.cool/simple/v1/cjs31m8me4kok0102bpw6if2u'
+})
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  const token = localStorage.getItem(GC_AUTH_TOKEN)
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : null
+    }
+  })
+
+  return forward(operation)
+})
+
+const apolloClient = new ApolloClient({  
+  link: authMiddleware.concat(httpLink),
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+  defaultOptions: {
+    $loadingKey: 'loading'
+  }
+})
+
+let userId = localStorage.getItem(GC_USER_ID)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  provide: apolloProvider.provide(),
+  router,
+  data: {
+    userId //set userId on the $root $data object
+  },
+  components: { App },
+  template: '<App/>'
+})
